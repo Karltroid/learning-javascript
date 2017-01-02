02c8ba5cf1ad1560515595d1504420a5232210e7
Karl <KarltroidContact@Gmail.com>
2017-01-02 12:13:07 -0500

    added jquery v. 3.1.1

---
+++ js/learningjquery/jquery.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+	// BASIC SELECTORS
+	$('body').css('border', '4px solid red');
+
+	// BASIC ANIMATIONS
+
+
+	// INDEX FILTER
+
+
+	// RELATIONSHIP FILTERS
+
+
+	// ATTRIBUTE FILTERS
+
+
+});
